b9cd424b370f8d5f15fb7ce3380bd77e5263e3a6
GOGoncalves <gustavo.goncalves@sptech.school>
2022-10-04 18:11:32 -0300

    Atualizacao Script Python

--- model/Libraries.py
+++ model/Libraries.py
@@ -1,51 +1,56 @@
 # Importações de Bibliotecas:
-import os, sys
+from ast import Str
+import pyodbc
+from mysql.connector import errorcode
+import mysql.connector
+import platform
+from datetime import datetime
+import time
+import psutil
+from multiprocessing import connection
+import os
+import sys
 
-bibliotecas = ['Str', 'psutil', 'time', 'datetime', 'platform', 'mysql-connector-python', 'pyodbc','sys']
+bibliotecas = ['Str', 'psutil', 'time', 'datetime','platform', 'mysql-connector-python', 'pyodbc', 'sys']
+
 
 def validLibrary(bibliotecas):
- for i in bibliotecas:
+    for i in bibliotecas:
         command = 'pip list | findstr /c:"{}"'.format(i)
         exibir = os.popen(command).read()
 
         if (exibir == ''):
-            print("Biblioteca", i ,"não encontrada!")
+            print("Biblioteca", i, "não encontrada!")
             ins = 'pip install {}'.format(i)
-            instal = os.system(ins)
-            
+            os.system(ins)
+
         else:
             print(exibir)
-            
+
+
+validLibrary(bibliotecas)
+
+
 def desinstalarBibliotecas(bibliotecas):
 
-    for i in bibliotecas:        
+    for i in bibliotecas:
         command = 'pip uninstall {}'.format(i)
         os.system(command)
         validLibrary(bibliotecas)
-        
-validLibrary(bibliotecas)
 
-from ast import Str
-import psutil
-import time
-from datetime import datetime
-import platform
-import mysql.connector
-from mysql.connector import errorcode
-import pyodbc
 def buscar_serial():
-	os_type = sys.platform.lower()
-	if "win" in os_type:
-		command = "wmic bios get serialnumber"
-	elif "linux" in os_type:
-		command = "hal-get-property --udi /org/freedesktop/Hal/devices/computer --key system.hardware.uuid"
-	elif "darwin" in os_type:
-		command = "ioreg -l | grep IOPlatformSerialNumber"
-	return os.popen(command).read().replace("\n","").replace("	","").replace(" ","")
+    os_type = sys.platform.lower()
+    if "win" in os_type:
+        command = "wmic bios get serialnumber"
+    elif "linux" in os_type:
+        command = "hal-get-property --udi /org/freedesktop/Hal/devices/computer --key system.hardware.uuid"
+    elif "darwin" in os_type:
+        command = "ioreg -l | grep IOPlatformSerialNumber"
+    return os.popen(command).read().replace("\n", "").replace("	", "").replace(" ", "")
 
-def captura(cursor):
+
+def captura(conn, cursor):
     print("Seja bem-vindo ao sistema de captura de dados do seu Hardware \U0001F604")
-
     tempo = 2
     numero = 10
 
@@ -57,22 +62,32 @@ def captura(cursor):
         nomeMaquina = meu_sistema.node
         modelo = meu_sistema.processor
         numero_serial = buscar_serial()
-        
-        numeros_serial = [numero_serial, "SerialNumberF2A3913", "SerialNumberB1M2932"]
-        nomemaquinas = [nomeMaquina, "Ubuntu", "Monterey"]
-        processadores = [modelo, "Intel Core i7-8650 CPU @ 1.90GHz","Intel Xeon Silver 4114 2.2GHz, 10C/20T, 9.6GT/s"]
-        arquiteturas = [arqmaquina, "Intel86","Intel86"]
-        sistemas = [sistema, "Linux", "MacOS"]
-        
+
+        codigosEquip = [sistema, arqmaquina,
+                        nomeMaquina, modelo, numero_serial]
+
+        numeros_serial = [codigosEquip[4],
+                          "SerialNumberF2A3913", "SerialNumberB1M2932"]
+        processadores = [codigosEquip[3], "Intel Core i7-8650 CPU @ 1.90GHz",
+                         "Intel Xeon Silver 4114 2.2GHz, 10C/20T, 9.6GT/s"]
+        nomemaquinas = [codigosEquip[2], "Ubuntu", "Monterey"]
+        arquiteturas = [codigosEquip[1], "Intel86", "Intel86"]
+        sistemas = [sistema[0], "Linux", "MacOS"]
+
         cursor.execute(
             "SELECT COUNT(idEquipamento) FROM `healthsystem`.`Equipamento`")
         row = cursor.fetchone()
         Equip = int(''.join(map(str, row)))
         print("\U0001F916 Equipamentos detectados:", Equip, "\n--------")
+        cursor.execute(
+            "SELECT COUNT(idParametro) FROM `healthsystem`.`Parametro`")
+        rowPam = cursor.fetchone()
+        Parametros = int(''.join(map(str, rowPam)))
+        print("\U0001F916 Parametros detectados:", Parametros)
 
         if Equip < 1:
             cursor.execute(
-                "SELECT COUNT(idLocal) FROM `healthsystem`.`local`")
+                "SELECT COUNT(idLocal) FROM `healthsystem`.`Local`")
             rowLocal = cursor.fetchone()
             Locais = int(''.join(map(str, rowLocal)))
             print("\U0001F916 Locais detectados:", Locais)
@@ -80,12 +95,32 @@ def captura(cursor):
                 i = i + 1
                 a = i - 1
                 cursor.execute("INSERT INTO `healthsystem`.`Equipamento` (fkFilial, fkLocal, serialNumber, nome,modelo,arqMaquina, sistemaOp) VALUES (%s,%s,%s, %s,%s, %s,%s);",
-                               (1000, i, numeros_serial[a],nomemaquinas[a], processadores[a], arquiteturas[a], sistemas[a]))
+                               (1000, i, numeros_serial[a], nomemaquinas[a], processadores[a], arquiteturas[a], sistemas[a]))
                 print("\U0001F916 Inserção de dados de Equipamento:", i)
                 conn.commit()
+        if Parametros < 1:
+            codigosHardware = [
+                "psutil.cpu_percent(interval=None", "percpu=False)','psutil.disk_usage('/').percent", "psutil.virtual_memory().percent"]
+
+            cursor.execute(
+                "SELECT COUNT(idComponente) FROM `healthsystem`.`Componente`")
+            rowComp = cursor.fetchone()
+            Componentes = int(''.join(map(str, rowComp)))
+
+            validacao = 0
+
+            for p in range(Equip):
+                p = p + 1
+                for i in range(Componentes):
+                    valor = codigosHardware[i]
+                    i = i + 1
+                    cursor.execute("INSERT INTO `healthsystem`.`Parametro` (fkEquipamento, fkComponente, codigo, valid) VALUES (%s,%s,%s,%s);",
+                                   (p, i, valor, validacao))
+
             leitura(cursor, numero, tempo)
         else:
             leitura(cursor, numero, tempo)
+
 
 def leitura(cursor, numero, tempo):
     Quantidade = range(numero)
@@ -93,8 +128,7 @@ def leitura(cursor, numero, tempo):
         dataHora = datetime.now()
         dataHoraFormat = dataHora.strftime('%Y/%m/%d %H:%M:%S')
 
-        percentualCPU = psutil.cpu_percent(
-            interval=None, percpu=False)
+        percentualCPU = psutil.cpu_percent(interval=None, percpu=False)
         percentualDisco = psutil.disk_usage('/').percent
         percentualMemoria = psutil.virtual_memory().percent
 
@@ -140,26 +174,32 @@ def leitura(cursor, numero, tempo):
                                (i, a, valor, dataHoraFormat))
                 conn.commit()
 
-        print("\U0001F4BB - Porcentagem de Utilização da CPU: {:.1f}%".format(percentualCPU),
-              "\n\U0001F4BB - Porcentagem de Utilização do Disco:", percentualDisco, '%',
-              "\n\U0001F4BB - Porcentagem de Utilização da Memoria:", percentualMemoria, '%',
-              "\n\U0001F55B - Data e Hora:", dataHoraFormat, "\n--------")
+            print("\U0001F4BB - Porcentagem de Utilização da CPU: {:.1f}%".format(percentualCPU),
+                  "\n\U0001F4BB - Porcentagem de Utilização do Disco:", percentualDisco, '%',
+                  "\n\U0001F4BB - Porcentagem de Utilização da Memoria:", percentualMemoria, '%',
+                  "\n\U0001F55B - Data e Hora:", dataHoraFormat, "\n--------")
 
-        conn.commit()
-        time.sleep(tempo)
+            conn.commit()
+            time.sleep(tempo)
     print("Captura de dados finalizada!")
-    
+    sair()
+
+def sair():
+    exit()
+
 # Validações banco local:
 
 def validacaoMysql(conn):
     print("Iniciando validações...")
-    
+
     cursor = conn.cursor()
 
-    cursor.execute("USE `HealthSystem`;")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS `healthsystem`;")
+
+    cursor.execute("USE `healthsystem`;")
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS `HealthSystem`.`Empresa` (
+    CREATE TABLE IF NOT EXISTS `healthsystem`.`Empresa` (
     `idEmpresa` INT NOT NULL AUTO_INCREMENT,
     `razaoSocial` VARCHAR(45) NOT NULL,
     `cnpj` CHAR(14) NOT NULL,
@@ -177,9 +217,9 @@ def validacaoMysql(conn):
     empresa = int(''.join(map(str, rowEmp)))
 
     if empresa < 1:
-        cursor.execute("INSERT INTO `HealthSystem`.`Empresa` VALUES (NULL,'PHILIPS DO BRASIL LTDA',61086336000103,'Avenida Marcos Penteado de Ulhoa Rodrigues',939,'Tambore','Barueri','SP','06460040');")
+        cursor.execute("INSERT INTO `healthsystem`.`Empresa` VALUES (NULL,'PHILIPS DO BRASIL LTDA',61086336000103,'Avenida Marcos Penteado de Ulhoa Rodrigues',939,'Tambore','Barueri','SP','06460040');")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Credencial` (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Credencial` (
     `idCredencial` INT NOT NULL,
     `tipoCredencial` VARCHAR(45) NOT NULL,
     `nivelPermissao` ENUM('1', '2', '3') NOT NULL,
@@ -195,7 +235,7 @@ def validacaoMysql(conn):
         cursor.execute(
             "INSERT INTO `healthsystem`.`Credencial` VALUES (323145,'Tecnico',1), (543221,'Analista',2), (386531,'Gerente',3);")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Usuario` (`fkEmpresa` INT NOT NULL,
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Usuario` (`fkEmpresa` INT NOT NULL,
   `idUsuario` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
@@ -206,12 +246,12 @@ def validacaoMysql(conn):
   INDEX `fk_Usuario_Credencial1_idx` (`fkcredencial` ASC) VISIBLE,
   CONSTRAINT `fk_Usuario_Empresa1`
     FOREIGN KEY(`fkEmpresa`)
-    REFERENCES `HealthSystem`.`Empresa` (`idEmpresa`)
+    REFERENCES `healthsystem`.`Empresa` (`idEmpresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Usuario_Credencial1`
     FOREIGN KEY(`fkcredencial`)
-    REFERENCES `HealthSystem`.`Credencial` (`idCredencial`)
+    REFERENCES `healthsystem`.`Credencial` (`idCredencial`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);""")
 
@@ -223,7 +263,7 @@ def validacaoMysql(conn):
         cursor.execute(
             "INSERT INTO `healthsystem`.`Usuario` VALUES (1,NULL,'fernandoBrandao','fernando.brandao@sptech.school','1234',323145);")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Componente` (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Componente` (
     `idComponente` INT NOT NULL AUTO_INCREMENT,
     `nomeComponente` VARCHAR(45) NOT NULL,
     PRIMARY KEY(`idComponente`)
@@ -236,9 +276,9 @@ def validacaoMysql(conn):
 
     if componente < 1:
         cursor.execute(
-            "INSERT INTO `HealthSystem`.`Componente` (`nomeComponente`) VALUES ('CPU'), ('Memoria'), ('Disco');")
+            "INSERT INTO `healthsystem`.`Componente` (`nomeComponente`) VALUES ('CPU'), ('Memoria'), ('Disco');")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Filial` (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Filial` (
     `idFilial` INT NOT NULL AUTO_INCREMENT,
     `fkEmpresa` INT NOT NULL,
     `nomeFantasia` VARCHAR(45) NOT NULL,
@@ -263,9 +303,9 @@ def validacaoMysql(conn):
 
     if filial < 1:
         cursor.execute(
-            "INSERT INTO `healthsystem`.`filial` VALUES (NULL,1,'HOSPITAL SAO LUIZ GONZAGA','R MICHEL OUCHANA',94,'JACANA','SÃO PAULO','SP','02276140');")
+            "INSERT INTO `healthsystem`.`Filial` VALUES (NULL,1,'HOSPITAL SAO LUIZ GONZAGA','R MICHEL OUCHANA',94,'JACANA','SÃO PAULO','SP','02276140');")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Local` (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Local` (
     `idLocal` INT NOT NULL AUTO_INCREMENT,
     `identificacao` VARCHAR(45) NOT NULL,
     PRIMARY KEY(`idLocal`)
@@ -279,7 +319,7 @@ def validacaoMysql(conn):
         cursor.execute(
             "INSERT INTO `healthsystem`.`Local` (`identificacao`) VALUES ('Sala de Ultrassom'),('Enfermaria'),('Sala de Manutenção');")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Equipamento` (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Equipamento` (
   `idEquipamento` INT NOT NULL AUTO_INCREMENT,
   `fkFilial` INT NOT NULL,
   `fkLocal` INT NOT NULL,
@@ -293,16 +333,16 @@ def validacaoMysql(conn):
   INDEX `fk_Equipamento_Filial1_idx` (`fkFilial` ASC) VISIBLE,
   CONSTRAINT `fk_Equipamento_Sala1`
     FOREIGN KEY (`fkLocal`)
-    REFERENCES `HealthSystem`.`Local` (`idLocal`)
+    REFERENCES `healthsystem`.`Local` (`idLocal`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Equipamento_Filial1`
     FOREIGN KEY (`fkFilial`)
-    REFERENCES `HealthSystem`.`Filial` (`idFilial`)
+    REFERENCES `healthsystem`.`Filial` (`idFilial`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);""")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Parametro` (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Parametro` (
   `fkEquipamento` INT NOT NULL,
   `fkComponente` INT NOT NULL,
   `idParametro` INT NOT NULL AUTO_INCREMENT,
@@ -313,17 +353,17 @@ def validacaoMysql(conn):
   INDEX `fk_Equipamento_has_Componente_Equipamento1_idx` (`fkEquipamento` ASC) VISIBLE,
   CONSTRAINT `fk_Equipamento_has_Componente_Equipamento1`
     FOREIGN KEY (`fkEquipamento`)
-    REFERENCES `HealthSystem`.`Equipamento` (`idEquipamento`)
+    REFERENCES `healthsystem`.`Equipamento` (`idEquipamento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Equipamento_has_Componente_Componente1`
     FOREIGN KEY (`fkComponente`)
-    REFERENCES `HealthSystem`.`Componente` (`idComponente`)
+    REFERENCES `healthsystem`.`Componente` (`idComponente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
     );""")
-    
-    cursor.execute("""CREATE TABLE IF NOT EXISTS `HealthSystem`.`Leitura` (
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `healthsystem`.`Leitura` (
   `fkEquipamento` INT NOT NULL,
   `fkComponente` INT NOT NULL,
   `idLeitura` INT NOT NULL AUTO_INCREMENT,
@@ -334,29 +374,30 @@ def validacaoMysql(conn):
   INDEX `fk_Equipamento_has_Componente_Equipamento1_idx` (`fkEquipamento` ASC) VISIBLE,
   CONSTRAINT `fk_Equipamento_has_Componente_Equipamento2`
     FOREIGN KEY (`fkEquipamento`)
-    REFERENCES `HealthSystem`.`Equipamento` (`idEquipamento`)
+    REFERENCES `healthsystem`.`Equipamento` (`idEquipamento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Equipamento_has_Componente_Componente2`
     FOREIGN KEY (`fkComponente`)
-    REFERENCES `HealthSystem`.`Componente` (`idComponente`)
+    REFERENCES `healthsystem`.`Componente` (`idComponente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
     );""")
 
     print("Validações de tabelas finalizada com sucesso.")
-    captura(cursor)
+    captura(conn, cursor)
 
 # Validações banco Cloud:
+
 
 def validacaoSql(conn):
     cursor = conn.cursor()
     print('Iniciando validações...')
-    
+
     captura(cursor)
-    
+
 try:
-# Deixando a variavel com mysql.local, sera setada a configuração de acesso no banco de dados Local MySQL
+    # Deixando a variavel com mysql.local, sera setada a configuração de acesso no banco de dados Local MySQL
     banco = "mysql.local"
 # Deixando a variavel com sql.nuvem, sera setada a configuração de acesso no banco de dados na nuvem SQL Server
     # banco = "sql.nuvem"
@@ -364,8 +405,7 @@ try:
         conn = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='qwea1020',
-            database='healthsystem'
+            password='qwea1020'
         )
         print("Conexão com o Banco de Dados MySQL efetuada com sucesso.")
         validacaoMysql(conn)
